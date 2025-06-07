1bdafcefa121c8d9e1e890982392a6e4b41969ea
alleng2 <berguo99@gmail.com>
2025-06-07 16:48:43 -0500

    Made Spring Boot backend skeleton.

---
+++ src/main/java/com/barcode/scanapi/ScanapiApplication.java
@@ -0,0 +1,13 @@
+package com.barcode.scanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScanapiApplication.class, args);
+	}
+
+}
